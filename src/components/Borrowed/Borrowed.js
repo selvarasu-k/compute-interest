ccbd89da35cfe84437406b3ea77232f465fd38ac
selvarasu-k <selvanewversion@gmail.com>
2023-07-27 17:27:42 +0530

    Updated borrowed and borrowedlist components

--- src/components/Borrowed/Borrowed.js
+++ src/components/Borrowed/Borrowed.js
@@ -8,75 +8,61 @@ const myBorrowList = [
         creditor: 'Vengadasalam',
         amount: 10000,
         interest: 2,
-        dateofissued: '01 Jun 2023',
+        dateofissued: new Date('06-01-2023'),
         description: 'Tamil Date Vaikaasi 18',
-        duedate: '01 Jun 2024'
+        duedate: new Date('06-01-2024')
     },
     {
         key: 'a2',
         creditor: 'Sinthamani',
         amount: 50000,
         interest: 1.5,
-        dateofissued: '25 Jan 2023',
+        dateofissued: new Date('01-25-2023'),
         description: 'Tamil date thai 11',
-        duedate: '01 Jun 2024'
+        duedate: new Date('01-25-2024')
     },
     {
         key: 'a3',
         creditor: 'Palanisamy BNP',
         amount: 10000,
         interest: 2,
-        dateofissued: '22 Nov 2022',
+        dateofissued: new Date('11-22-2022'),
         description: 'Tamil date karthikai 6',
-        duedate: '22 Nov 2023'
+        duedate: new Date('11-22-2023')
     },
     {
         key: 'a4',
         creditor: 'Lakshmi Amma',
         amount: 10000,
         interest: 2,
-        dateofissued: '28 Oct 2022',
+        dateofissued: new Date('10-28-2022'),
         description: 'Tamil date aippasi 11',
-        duedate: '28 Oct 2023'
+        duedate: new Date('10-28-2023')
     },
     {
         key: 'a5',
         creditor: 'Vathiyar',
         amount: 100000,
         interest: 1.5,
-        dateofissued: '15 Feb 2023',
+        dateofissued: new Date('02-15-2023'),
         description: 'Tamil date Maasi 03',
-        duedate: '15 Feb 2024'
+        duedate: new Date('02-15-2024')
     },
     {
         key: 'a6',
         creditor: 'Periamma',
         amount: 10000,
         interest: 2,
-        dateofissued: '31 Oct 2022',
+        dateofissued: new Date('10-31-2022'),
         description: 'Tamil date aippasi 14',
-        duedate: '31 Oct 2023'
+        duedate: new Date('10-31-2023')
     }
 ];
 
 const Borrowed = () => {
-
-    const BorrowData = myBorrowList.map(listitem => {
-        return (
-                <tr>
-                    <td key={listitem.key}>{listitem.creditor}</td>
-                    <td key={listitem.key}>₹ {listitem.amount}</td>
-                    <td key={listitem.key}>{listitem.interest}</td>
-                    <td key={listitem.key}>{listitem.dateofissued}</td>
-                    <td key={listitem.key}>{listitem.description}</td>
-                    <td key={listitem.key}>{listitem.duedate}</td>
-                </tr>
-            )
-        }
-    );
     return (
         <div>
-            <BorrowedList passBorrowData = {BorrowData} />
+            <BorrowedList passBorrowData = {myBorrowList} />
         </div>
     ) 
 }
--- src/components/Borrowed/BorrowedList.js
+++ src/components/Borrowed/BorrowedList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BorrowedListItem from "./BorrowedListItem";
 import './BorrowedList.css';
 
 const BorrowedList = (props) => {
@@ -17,7 +18,19 @@ const BorrowedList = (props) => {
                         <th>Description</th>
                         <th>Due Date</th>
                     </tr>
-                    {props.passBorrowData}
+                    {props.passBorrowData.map(listitem => {
+                        return (
+                            <BorrowedListItem 
+                                key={listitem.key}
+                                creditor={listitem.creditor}
+                                amount={listitem.amount}
+                                interest={listitem.interest}
+                                dateofissued={listitem.dateofissued}
+                                description={listitem.description}
+                                duedate={listitem.duedate}
+                            />
+                        )}
+                    )}
                 </table>
             </div>
         </div>
